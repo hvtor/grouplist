dd3c3e7c8bb017d79cc6bc7d283a1f4a763e470b
AshBT <ashley.beattie@gmail.com>
2013-03-25 19:59:38 -0400

    initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grouplist::Application.config.secret_token = 'e79b4ee1278e8b3f270857f817c55405038119d0e5953d0b5db5d29a53d2b5f543c70f0771baef39084a64dde48d6b687d9fc0282662087b2460695bcdcfe4cc'
